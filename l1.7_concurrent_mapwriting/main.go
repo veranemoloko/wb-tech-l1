356b774dc8188d8d30da17571b6f75d6933e6e16
Verochka <neveranemoloko@gmail.com>
2025-08-28 21:23:07 +0000

    add comment 1.7

--- l1.7_concurrent_mapwriting/main.go
+++ l1.7_concurrent_mapwriting/main.go
@@ -5,35 +5,26 @@ import (
 	"sync"
 )
 
-type squareMap struct {
-	mu sync.Mutex
-	mp map[int]int
-}
-
-func (sm *squareMap) Write(key int, value int) {
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
-	sm.mp[key] = value
-}
-
 func main() {
-	sm := squareMap{mp: make(map[int]int)}
+	// declare a concurrent-safe map
+	var sm sync.Map
 	var wg sync.WaitGroup
 
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
+	// launch a goroutine for each number to write its square to sm
 	for i := range nums {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			sm.Write(i, i*i)
-		}(i)
+			sm.Store(i, i*i) // store square in sm
+		}(i) // pass i as parameter to avoid closure capture issue
 	}
 
 	wg.Wait()
 
-	for k, v := range sm.mp {
-		fmt.Printf("%d: %d\n", k, v)
-	}
-
+	sm.Range(func(key, value any) bool {
+		fmt.Printf("%d: %d\n", key, value)
+		return true // return true to continue iteration
+	})
 }
