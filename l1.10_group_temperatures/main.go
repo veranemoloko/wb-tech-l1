12b7ec92b27ab8c2941fb2fb7fbde50759f8c431
Verochka <neveranemoloko@gmail.com>
2025-09-03 19:01:25 +0000

    add task l1.10

---
+++ l1.10_group_temperatures/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	data := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5, 0.0}
+
+	groupData := make(map[int][]float64)
+
+	var step int
+
+	for _, t := range data {
+		step = int(t/10) * 10
+		groupData[step] = append(groupData[step], t)
+
+	}
+
+	fmt.Println(groupData)
+
+}
