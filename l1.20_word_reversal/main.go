f023304b97cd1d28f6e1548add9dd5836f7da478
Verochka <neveranemoloko@gmail.com>
2025-09-07 14:27:12 +0000

    add l1.20

---
+++ l1.20_word_reversal/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reverseString(s string) string {
+	words := strings.Fields(s)
+	// swap the i-th word with its counterpart from the end
+	for i := 0; i < len(words)/2; i++ {
+		words[i], words[len(words)-1] = words[len(words)-1], words[i]
+	}
+
+	return strings.Join(words, " ")
+
+}
+
+func main() {
+	str := "dog with me on the Moon"
+	fmt.Println(reverseString(str))
+
+}
