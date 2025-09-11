a45871aff86ed6c5f04014466e32bd0cf43f5a83
root <root@VERANEMOLOKO.>
2025-09-11 21:18:07 +0300

    add new l1.24

--- l1.24_distance_between_points/main.go
+++ l1.24_distance_between_points/main.go
@@ -14,7 +14,7 @@ func NewPoint(x, y float64) Point {
 	return Point{x: x, y: y}
 }
 
-func DistancePoints(p1, p2 Point) float64 {
+func (p1 Point) Distance(p2 Point) float64 {
 	dx := p1.x - p2.x
 	dy := p1.y - p2.y
 	return math.Sqrt(dx*dx + dy*dy)
@@ -25,5 +25,5 @@ func main() {
 	p1 := NewPoint(1.2, 2.2)
 	p2 := NewPoint(14.2, -6.3)
 
-	fmt.Printf("Distance: %.2f\n", DistancePoints(p1, p2))
+	fmt.Printf("Distance: %.2f\n", p1.Distance(p2))
 }
