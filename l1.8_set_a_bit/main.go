c6570896c9155f60dca8691a2334df001fa07165
root <root@VERANEMOLOKO.>
2025-09-11 20:53:38 +0300

    add new l1.8

--- l1.8_set_a_bit/main.go
+++ l1.8_set_a_bit/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func setBit(num *int, i uint, bit uint8) {
+func setBitInt64(num *int64, i uint, bit uint8) {
 	switch bit {
 	case 1:
 		*num = *num | (1 << i)
@@ -16,13 +16,13 @@ func setBit(num *int, i uint, bit uint8) {
 }
 
 func main() {
-	var num int = 259
+	var num int64 = 259
 	fmt.Printf("number %d: %14b\n", num, num)
 
-	setBit(&num, 0, 0)
+	setBitInt64(&num, 0, 0)
 	fmt.Printf("zero bit to 0: %11b\n", num)
 
-	setBit(&num, 2, 1)
+	setBitInt64(&num, 2, 1)
 	fmt.Printf("second bit to 1: %b\n", num)
 
 }
